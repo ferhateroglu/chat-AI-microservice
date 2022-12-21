75572191b16e56a137c3708687a2b6a948f2f408
Ferhat Eroğlu <erogluferhat@outlook.com>
2022-12-22 00:10:41 +0300

    added mail service proxy

--- gateway/index.js
+++ gateway/index.js
@@ -8,7 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 //====== microservices ======
+app.use("/mail",proxy("http://localhost:8082"))//mail
 app.use("/",proxy("http://localhost:8081"))//account
+
 
 app.listen(8080,()=>{
     console.log("Gatway running on 8080")
