67465eebaf670034d4cf4ea9f61ff70095fc235b
Ferhat Eroğlu <erogluferhat@outlook.com>
2022-12-30 21:53:20 +0300

    added syncIndexes

--- account/src/database/connection.js
+++ account/src/database/connection.js
@@ -3,8 +3,10 @@ const {DB_URL} = require("../config");
 
 module.exports = async () => {
     try{
+        
         mongoose.set("strictQuery", false);
         await mongoose.connect(DB_URL);
+        mongoose.syncIndexes();
         console.log("connected to mongodb")
     } catch (err){
         console.log("db connection err")
